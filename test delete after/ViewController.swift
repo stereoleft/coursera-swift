f19f5502e5d89cc3360e70f9fa6cbf105082df47
Luis Romero <luisromero@luisrmac-1gm0th.local>
2016-01-26 17:17:59 -0800

    //This is a comment

--- test delete after/ViewController.swift
+++ test delete after/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+//This is a comment
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
